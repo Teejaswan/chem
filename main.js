c4a8d6fa02bd055623b6eb68b02ac0ace92173b8
Rajaniraiyn <rajaniraiyn@gmail.com>
2022-03-03 21:52:07 +0545

    Fix MPA issues

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-import './style.css'
-
-import './pages/page1.html'
-import './pages/page2.html'
-import './pages/page3.html'
-import './pages/page4.html'
-import './pages/page5.html'
+import './style.css';
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        page1: resolve(__dirname, "pages/page1/index.html"),
+        page2: resolve(__dirname, "pages/page2/index.html"),
+        page3: resolve(__dirname, "pages/page3/index.html"),
+        page4: resolve(__dirname, "pages/page4/index.html"),
+        page5: resolve(__dirname, "pages/page5/index.html"),
+      },
+    },
+  },
 });
